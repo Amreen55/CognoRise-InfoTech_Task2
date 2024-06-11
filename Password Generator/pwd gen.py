8eddffdc9f6444269d58e51e7740b9b0968d1f1e
Amreen55 <152794809+Amreen55@users.noreply.github.com>
2024-06-11 19:28:15 +0530

    Update pwd gen.py

--- Password Generator/pwd gen.py
+++ Password Generator/pwd gen.py
@@ -1,27 +1,74 @@
 import string
 import random
-length = int(input("Enter password length:"))
-print('''Choose character set for password from these:
-      1.Digits
-      2.Letters
-      3.Special characters
-      4.Exit''')
-characterList = ""
-while(True):
-    choice = int(input("Pick a number:"))
-    if(choice == 1):
-        characterList += string.ascii_letters10
-    elif(choice == 2):
-         characterList +=string.digits
-    elif(choice == 3):
-         characterList += string.punctuation
-    elif(choice == 4):
-           break
-    else:
-        print("Please pick a valid option!")
-    password = []
-    for i in range(length):
-        randomchar=random.choice(characterList)
-        password.append(randomchar)
-        print("The random password is "+"".join(password))
+
+
+# store all characters in lists 
+s1 = list(string.ascii_lowercase)
+s2 = list(string.ascii_uppercase)
+s3 = list(string.digits)
+s4 = list(string.punctuation)
+
+
+# Ask user about the number of characters
+user_input = input("How many characters do you want in your password? ")
+
+
+# check this input is it number? is it more than 8?
+while True:
+
+	try:
+
+		characters_number = int(user_input)
+
+		if characters_number < 8:
+
+			print("Your number should be at least 8.")
+
+			user_input = input("Please, Enter your number again: ")
+
+		else:
+
+			break
+
+	except:
+
+		print("Please, Enter numbers only.")
+
+		user_input = input("How many characters do you want in your password? ")
+
+
+# shuffle all lists
+random.shuffle(s1)
+random.shuffle(s2)
+random.shuffle(s3)
+random.shuffle(s4)
+
+
+# calculate 30% & 20% of number of characters
+part1 = round(characters_number * (30/100))
+part2 = round(characters_number * (20/100))
+
+
+# generation of the password (60% letters and 40% digits & punctuations)
+result = []
+
+for x in range(part1):
+
+	result.append(s1[x])
+	result.append(s2[x])
+
+for x in range(part2):
+
+	result.append(s3[x])
+	result.append(s4[x])
+
+
+# shuffle result
+random.shuffle(result)
+
+
+# join result
+password = "".join(result)
+print("Strong Password: ", password)
+
             
